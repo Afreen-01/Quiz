9439e5ac5671aacfaa60a829f8b3e08368c7d037
Afreen <ashiyabano0803@gmail.com>
2024-12-29 15:20:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
+// script.js
+const quizData = [
+    {
+        question: "What is your favorite color?",
+        answers: {
+            a: "Red",
+            b: "Blue",
+            c: "Green",
+            d: "Yellow"
+        },
+        correct: "b"
+    },
+    {
+        question: "What is your favorite animal?",
+        answers: {
+            a: "Dog",
+            b: "Cat",
+            c: "Elephant",
+            d: "Lion"
+        },
+        correct: "a"
+    }
+];
 
+function buildQuiz() {
+    const quizContainer = document.getElementById('quiz');
+    const output = [];
+
+    quizData.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+        for (letter in currentQuestion.answers) {
+            answers.push(
+                `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} : ${currentQuestion.answers[letter]}
+                </label>`
+            );
+        }
+
+        output.push(
+            `<div class="question">${currentQuestion.question}</div>
+             <div class="answers">${answers.join('')}</div>`
+        );
+    });
+
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults() {
+    const answerContainers = document.querySelectorAll('.answers');
+    let numCorrect = 0;
+
+    quizData.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if (userAnswer === currentQuestion.correct) {
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'green';
+        } else {
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = `${numCorrect} out of ${quizData.length}`;
+}
+
+document.getElementById('submit').addEventListener('click', showResults);
+
+buildQuiz();
